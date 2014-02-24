9b44675ae4a2dbd4f0c73b512918c8c03d601910
Naveed Ahmad <naveed.ahmad@nytimes.com>
2014-02-23 19:31:09 -0500

    .emacs.d/lisp-config.el

--- .emacs.d/lisp-config.el
+++ .emacs.d/lisp-config.el
@@ -3,6 +3,9 @@
 (add-to-list 'load-path "~/Documents/lisp/slime")
 (require 'slime-autoloads)
 
+
 ;; Set your lisp system and, optionally, some contribs
 (setq inferior-lisp-program "/usr/local/bin/sbcl")
 
+(setq scheme-program-name "/usr/local/bin/mit-scheme")
+(require 'xscheme)
